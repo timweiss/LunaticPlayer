cb79f8c32540aeec3bc084443122456bdf217d42
Tim W <tim@weiss.moe>
2017-03-19 17:53:55 +0100

    Quick fix

--- LunaticPlayer.GRadioAPI/Classes/Song.cs
+++ LunaticPlayer.GRadioAPI/Classes/Song.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
+using Newtonsoft.Json;
 
 namespace LunaticPlayer.Classes
 {
@@ -31,6 +32,7 @@ namespace LunaticPlayer.Classes
 
         public string CircleArtist => $"{ArtistName} ({CircleName})";
 
+        [JsonIgnore]
         public BitmapImage AlbumArt { get; set; }
     }
 }
