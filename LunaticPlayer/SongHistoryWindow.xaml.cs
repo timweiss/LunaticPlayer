4f513c1ffd040075b6db901911be820de1729f60
Tim W <tim@weiss.moe>
2017-03-23 20:34:56 +0100

    Visual changes

--- LunaticPlayer/SongHistoryWindow.xaml.cs
+++ LunaticPlayer/SongHistoryWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using LunaticPlayer.Player;
 
 namespace LunaticPlayer
@@ -31,6 +19,9 @@ namespace LunaticPlayer
             PopulateList();
         }
 
+        /// <summary>
+        /// Fills the ListBox with <see cref="Classes.Song"/> items.
+        /// </summary>
         private void PopulateList()
         {
             //foreach (var song in _songHistory.SongHistory)
