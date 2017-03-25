62bfd19db791b2f4d542e88751cbc45773985adb
Tim W <tim@weiss.moe>
2017-03-25 12:13:25 +0100

    Fixed AssemblyInfo

--- LunaticPlayer.GRadioAPI/Properties/AssemblyInfo.cs
+++ LunaticPlayer.GRadioAPI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("LunaticPlayer.GSRadioAPI")]
+[assembly: AssemblyTitle("LunaticPlayer.GRadioAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LunaticPlayer.GSRadioAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("LunaticPlayer.GRadioAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Tim Weiß")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
